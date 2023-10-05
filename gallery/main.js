704c9c671a815de72319535c808ea9041c0314f0
addyxx-h4ck1 <briannjosh23@gmail.com>
2023-10-05 06:24:14 +0300

    First commit

---
+++ gallery/main.js
@@ -0,0 +1,25 @@
+const response = fetch ('packageA.json')
+
+const myFunc = async () => {
+    try {
+        const data = await response;
+        const dataJson = await data.json();
+        const urlArray = dataJson.images;
+       
+
+        const parent = document.querySelector('.main')
+        urlArray.forEach(element => {
+            const a = document.createElement('a')
+            a.setAttribute(`href`, `${element}`)
+
+            const image = document.createElement('img')
+            image.setAttribute(`src`, `${element}`)
+
+            a.appendChild(image)
+            parent.appendChild(a)
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+myFunc();
