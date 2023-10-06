aa8c74ba635425bbd9c1572eaf7365a1b331a050
addyxx-h4ck1 <briannjosh23@gmail.com>
2023-10-06 14:28:17 +0300

    first commit

---
+++ validate.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('form');
+
+let name = document.getElementById ('name')
+console.log(name);
+
+let email = document.getElementById ('email')
+console.log(email);
+
+let subject = document.getElementById ('subject')
+console.log(subject);
+
+let message = document.getElementById ('message')
+console.log(message);
+let submit = document.querySelector('submit');
+let sent = document.querySelector('.sent');
+console.log(sent);
+
